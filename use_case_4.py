c15fa1047f4d846390477c73cc727dd5a362a652
gegiske <gegiske@stud.ntnu.no>
2026-03-12 11:48:18 +0100

    feat: Add date input to weekly schedule function

--- use_case_4.py
+++ use_case_4.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 # opens the file
-def getWeeklySchedule():
+def getWeeklySchedule(start_date, end_date):
     with sqlite3.connect("DB2.db") as con:
         cur = con.cursor()
         query = """
@@ -10,10 +10,10 @@ def getWeeklySchedule():
         WHERE time BETWEEN ? AND ?
         ORDER BY time
         """
-        cur.execute(query, ("2025-03-16", "2025-03-23"))
+        cur.execute(query, (start_date, end_date))
         # To get a prettier output:
         rows = cur.fetchall()
         for row in rows:
             print(row)
 
-getWeeklySchedule()
+getWeeklySchedule("2025-03-16", "2025-03-23")
