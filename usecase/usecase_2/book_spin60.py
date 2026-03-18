62d46a3d756735bd4d260a643e3cc0fd28e3deee
Eline Jåsund <elinejaa@stud.ntnu.no>
2026-03-18 11:03:17 +0100

    chore: fix format for usecase3 and refactor usecase2

--- usecase/usecase_2/book_spin60.py
+++ usecase/usecase_2/book_spin60.py
@@ -1,24 +1,24 @@
 import sqlite3
 
-def book_group_lesson(member: str, activity: str, time: str):
-    with sqlite3.connect("DB2.db") as con:
+def find_group_lesson(activity: str, time: str, center: str):
+     with sqlite3.connect("DB2.db") as con:
         cursor = con.cursor()
 
-        #Find member
-        cursor.execute("SELECT id FROM member WHERE mail = ?", (member,))
-        member_row = cursor.fetchone()
-        if not member_row:
-            print("User not found")
-            return
-        member_id = member_row[0]
-
-        # Find activity_type_id
+        #Find activity_type_id
         cursor.execute("SELECT id FROM activity_type WHERE name = ?", (activity,))
         activity_row = cursor.fetchone()
         if not activity_row:
             print("Activity not found")
             return
         activity_id = activity_row[0]
+
+        #Find center
+        cursor.execute("SELECT id FROM center WHERE name = ?", (center,))
+        center_row = cursor.fetchone()
+        if not center_row:
+            print("Center not found")
+            return
+        center_id = center_row[0]
 
         # Find group_lesson
         cursor.execute("""
@@ -27,14 +27,32 @@ def book_group_lesson(member: str, activity: str, time: str):
         JOIN room ON group_lesson.room_id = room.id
         JOIN center ON room.center_id = center.id
         JOIN lesson_types ON group_lesson.id = lesson_types.lesson_id
-        WHERE group_lesson.time = ? AND lesson_types.activity_type_id = ?
-        """, (time, activity_id))
+        WHERE group_lesson.time = ? AND center.id = ? AND lesson_types.activity_type_id = ?
+        """, (time, center_id, activity_id))
 
         lesson_row = cursor.fetchone()
         if not lesson_row:
             print("Training not found")
-            return
+            return None, None
         lesson_id, capacity = lesson_row
+
+        return lesson_id, capacity
+
+def book_group_lesson(member: str, lesson_id: int, capacity: int):
+    with sqlite3.connect("DB2.db") as con:
+        cursor = con.cursor()
+
+        # Find member
+        cursor.execute("SELECT id FROM member WHERE mail = ?", (member,))
+        member_row = cursor.fetchone()
+        if not member_row:
+            print("User not found")
+            return
+        member_id = member_row[0]
+
+        if not lesson_id:
+            print("Lesson not found")
+            return
 
         # Check current participants
         cursor.execute("SELECT COUNT(*) FROM group_lesson_booking WHERE group_lesson_id = ?", (lesson_id,))
@@ -56,4 +74,5 @@ def book_group_lesson(member: str, activity: str, time: str):
 
         print("Booking successful")
 
-book_group_lesson("johnny@stud.ntnu.no", "Spin60", "2025-03-17 18:30")
+lesson_id, capacity = find_group_lesson("Spin60", "2025-03-17 18:30", "Øya treningssenter")
+book_group_lesson("johnny@stud.ntnu.no", lesson_id, capacity)
