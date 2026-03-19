cddce071863f64e8bbbed284d669ac45d6b28ff6
gegiske <gegiske@stud.ntnu.no>
2026-03-19 08:28:34 +0100

    chore: Change print for use case 5

--- usecase/usecase_5/use_case_5.py
+++ usecase/usecase_5/use_case_5.py
@@ -5,7 +5,7 @@ with sqlite3.connect("DB2.db") as con:
     cur = con.cursor()
 
     query = """
-    SELECT group_lesson_participates.member_id, group_lesson_participates.date, activity_type.name FROM group_lesson_participates
+    SELECT activity_type.name, group_lesson_participates.date FROM group_lesson_participates
     JOIN lesson_types ON group_lesson_participates.group_lesson_id = lesson_types.lesson_id
     JOIN activity_type ON lesson_types.activity_type_id = activity_type.id
     JOIN member ON group_lesson_participates.member_id = member.id
