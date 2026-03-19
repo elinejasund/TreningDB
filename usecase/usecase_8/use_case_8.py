a25022cc2e216ad1be4e38bd7d10408f84012fd6
Eline Jåsund <elinejaa@stud.ntnu.no>
2026-03-19 10:03:19 +0100

    chore: add python file for executing sql

---
+++ usecase/usecase_8/use_case_8.py
@@ -0,0 +1,24 @@
+import sqlite3
+import os
+
+def execute_file(path):
+    base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
+    
+    db_path = os.path.join(base_path, "DB2.db")
+
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+
+    with open(path, "r", encoding="utf-8") as fil:
+        sql = fil.read()
+
+    cursor.execute(sql)
+    results = cursor.fetchall()
+
+    # Print results nicely
+    for row in results:
+     print(f"Member1: {row[0]} Member2: {row[1]} Amount: {row[2]}")
+
+    conn.close()
+
+execute_file("usecase/usecase_8/use_case_8.sql")
