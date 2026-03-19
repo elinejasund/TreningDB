ee49bfbf8af682b4eb6ff89e2fa2600006e57ccd
Eline Jåsund <elinejaa@stud.ntnu.no>
2026-03-19 11:13:59 +0100

    fix: remove uncecessary lines from file

--- usecase/usecase_7/attender.sql
+++ usecase/usecase_7/attender.sql
@@ -1,17 +1,3 @@
-WITH member_ids AS (
-    SELECT id, mail FROM member
-),
-
-lesson_ids AS (
-    SELECT id AS lesson_id, time FROM group_lesson
-)
-
-INSERT OR IGNORE INTO group_lesson_participates(member_id, group_lesson_id, date)
-SELECT m.id, l.lesson_id, t.lesson_time
-FROM attendance_plan t
-JOIN member_ids m ON m.mail = t.mail
-JOIN lesson_ids l ON l.time LIKE t.lesson_time || '%';
-
 WITH attendance AS (
     SELECT member_id, COUNT(*) AS total_attendances
     FROM group_lesson_participates
