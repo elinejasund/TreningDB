32b7e2e99bcc9d11f15cee4222f96ef2107d883a
Eline Jåsund <elinejaa@stud.ntnu.no>
2026-03-19 09:44:19 +0100

    add python files for data and table construction

--- table_construction/table_construction.sql
+++ table_construction/table_construction.sql
@@ -1,4 +1,6 @@
 -- member 
+DROP TABLE IF EXISTS member;
+
 CREATE TABLE member ( 
   id INTEGER NOT NULL PRIMARY KEY, 
   name TEXT NOT NULL, 
@@ -6,25 +8,39 @@ CREATE TABLE member (
   phone_number TEXT NOT NULL 
 );
 
--- group lesson participates
-CREATE TABLE group_lesson_participates ( 
-  member_id INTEGER NOT NULL,
-  group_lesson_id INTEGER NOT NULL,
-  date DATETIME NOT NULL, -- date i lesson gjelder ikke her siden dette registrerer oppmøte
-  FOREIGN KEY (member_id) REFERENCES member(id),
-  FOREIGN KEY (group_lesson_id) REFERENCES group_lesson(id),
-  PRIMARY KEY (member_id, group_lesson_id, date) 
-  );
-  
--- penalties 
-CREATE TABLE penalties ( 
-  member_id INTEGER NOT NULL, 
-  date DATETIME NOT NULL, 
-  FOREIGN KEY (member_id) REFERENCES member(id),
-  PRIMARY KEY (member_id, date) 
-); 
+-- center
+DROP TABLE IF EXISTS center;
+
+CREATE TABLE center (
+    id INTEGER PRIMARY KEY NOT NULL,
+    address TEXT NOT NULL,
+    name TEXT NOT NULL,
+    has_group_lesson BOOLEAN NOT NULL,
+    has_self_training BOOLEAN NOT NULL
+);
+
+-- room
+DROP TABLE IF EXISTS room;
+
+CREATE TABLE room (
+    id INTEGER PRIMARY KEY NOT NULL,
+    center_id INTEGER NOT NULL,
+    type TEXT NOT NULL,
+    capacity INTEGER NOT NULL,
+    FOREIGN KEY (center_id) REFERENCES center(id)
+);
+
+-- sports team
+DROP TABLE IF EXISTS sports_team;
+
+CREATE TABLE sports_team (
+  id INTEGER NOT NULL PRIMARY KEY,
+  name TEXT NOT NULL
+);
 
 -- group lessons 
+DROP TABLE IF EXISTS group_lesson;
+
 CREATE TABLE group_lesson ( 
   id INTEGER NOT NULL PRIMARY KEY, 
   room_id INTEGER NOT NULL, 
@@ -35,7 +51,54 @@ CREATE TABLE group_lesson (
   FOREIGN KEY (instructor) REFERENCES member(id)
 );
 
--- activity types 
+-- bike
+DROP TABLE IF EXISTS bike;
+
+CREATE TABLE bike (
+  number INTEGER NOT NULL PRIMARY KEY,
+  room INTEGER NOT NULL,
+  type TEXT NOT NULL,
+  has_bluetooth BOOLEAN NOT NULL,
+  FOREIGN KEY (room) REFERENCES room(id)
+);
+
+-- treadmill
+DROP TABLE IF EXISTS treadmill;
+
+CREATE TABLE treadmill (
+  number INTEGER NOT NULL PRIMARY KEY,
+  room INTEGER NOT NULL,
+  manufacturer TEXT NOT NULL,
+  max_speed INTEGER NOT NULL,
+  max_rise INTEGER NOT NULL,
+  FOREIGN KEY (room) REFERENCES room(id)
+);
+
+-- group lesson participates
+DROP TABLE IF EXISTS group_lesson_participates;
+
+CREATE TABLE group_lesson_participates ( 
+  member_id INTEGER NOT NULL,
+  group_lesson_id INTEGER NOT NULL,
+  date DATETIME NOT NULL, -- date i lesson gjelder ikke her siden dette registrerer oppmøte
+  FOREIGN KEY (member_id) REFERENCES member(id),
+  FOREIGN KEY (group_lesson_id) REFERENCES group_lesson(id),
+  PRIMARY KEY (member_id, group_lesson_id, date) 
+  );
+  
+-- penalties 
+DROP TABLE IF EXISTS penalties;
+
+CREATE TABLE penalties ( 
+  member_id INTEGER NOT NULL, 
+  date DATETIME NOT NULL, 
+  FOREIGN KEY (member_id) REFERENCES member(id),
+  PRIMARY KEY (member_id, date) 
+); 
+
+-- activity types
+DROP TABLE IF EXISTS activity_type;
+
 CREATE TABLE activity_type ( 
   id INTEGER NOT NULL PRIMARY KEY,
   name TEXT NOT NULL,
@@ -43,6 +106,8 @@ CREATE TABLE activity_type (
 );
 
 -- lesson_types
+DROP TABLE IF EXISTS lesson_types;
+
 CREATE TABLE lesson_types (
   lesson_id INTEGER NOT NULL,
   activity_type_id INTEGER NOT NULL,
@@ -51,25 +116,10 @@ CREATE TABLE lesson_types (
   FOREIGN KEY (activity_type_id) REFERENCES activity_type(id)
 );
 
--- center
-CREATE TABLE center (
-    id INTEGER PRIMARY KEY NOT NULL,
-    address TEXT NOT NULL,
-    name TEXT NOT NULL,
-    has_group_lesson BOOLEAN NOT NULL,
-    has_self_training BOOLEAN NOT NULL
-);
-
--- room
-CREATE TABLE room (
-    id INTEGER PRIMARY KEY NOT NULL,
-    center_id INTEGER NOT NULL,
-    type TEXT NOT NULL,
-    capacity INTEGER NOT NULL,
-    FOREIGN KEY (center_id) REFERENCES center(id)
-);
-
 -- center_hours
+DROP TABLE IF EXISTS opening_hours;
+DROP TABLE IF EXISTS staffing_hours;
+
 CREATE TABLE opening_hours (
     center_id INTEGER NOT NULL,
     weekday TEXT CHECK(weekday IN ('mandag','tirsdag','onsdag','torsdag','fredag','lørdag','søndag')),
@@ -89,6 +139,8 @@ CREATE TABLE staffing_hours (
 );
 
 -- facility
+DROP TABLE IF EXISTS facility;
+
 CREATE TABLE facility (
     center_id INTEGER NOT NULL,
     type TEXT NOT NULL,
@@ -97,6 +149,8 @@ CREATE TABLE facility (
 );
 
 -- visit
+DROP TABLE IF EXISTS visit;
+
 CREATE TABLE visit (
     member_id INTEGER NOT NULL,
     center_id INTEGER NOT NULL,
@@ -106,13 +160,9 @@ CREATE TABLE visit (
     FOREIGN KEY (member_id) REFERENCES member(id)
 );
 
--- sports team
-CREATE TABLE sports_team (
-  id INTEGER NOT NULL PRIMARY KEY,
-  name TEXT NOT NULL
-);
-
 -- sports team members
+DROP TABLE IF EXISTS sports_team_members;
+
 CREATE TABLE sports_team_members (
   team_id INTEGER NOT NULL,
   member_id INTEGER NOT NULL,
@@ -121,26 +171,9 @@ CREATE TABLE sports_team_members (
   PRIMARY KEY (team_id, member_id)
 );
 
--- bike
-CREATE TABLE bike (
-  number INTEGER NOT NULL PRIMARY KEY,
-  room INTEGER NOT NULL,
-  type TEXT NOT NULL,
-  has_bluetooth BOOLEAN NOT NULL,
-  FOREIGN KEY (room) REFERENCES room(id)
-);
-
--- treadmill
-CREATE TABLE treadmill (
-  number INTEGER NOT NULL PRIMARY KEY,
-  room INTEGER NOT NULL,
-  manufacturer TEXT NOT NULL,
-  max_speed INTEGER NOT NULL,
-  max_rise INTEGER NOT NULL,
-  FOREIGN KEY (room) REFERENCES room(id)
-);
-
 -- room booking
+DROP TABLE IF EXISTS room_booking;
+
 CREATE TABLE room_booking (
   id INTEGER NOT NULL PRIMARY KEY,
   room INTEGER NOT NULL,
@@ -151,6 +184,9 @@ CREATE TABLE room_booking (
 );
 
 -- team room booking participates
+DROP TABLE IF EXISTS team_room_booking_participates;
+DROP TABLE IF EXISTS group_lesson_booking;
+
 CREATE TABLE team_room_booking_participates (
   member_id INTEGER NOT NULL,
   room_booking_id INTEGER NOT NULL,
@@ -170,6 +206,8 @@ CREATE TABLE group_lesson_booking (
 );
 
 -- waitlist
+DROP TABLE IF EXISTS waitlist;
+
 CREATE TABLE waitlist (
   member_id INTEGER NOT NULL,
   position INTEGER NOT NULL,
